f1296eeb14750039b63c7fa46a338363e518c4bb
Allison Dobbs <104958198+A11isonD@users.noreply.github.com>
2022-08-01 21:55:30 -0400

    added submit button functionality

---
+++ home.js
@@ -0,0 +1,6 @@
+const form=document.querySelector("#contact_form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let name=event.target[0].value;
+    document.querySelector("#success").innerText= `Thanks, ${name}!  Your message has been sent!`  
+})
